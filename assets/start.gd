427b66477fe068d5447e57557196f25e24838003
ahpham123 <ahpham123@csu.fullerton.edu>
2025-04-13 02:20:44 -0700

    Start Screen, need enemy assets, button is functional, changed main scene to start scene

---
+++ assets/start.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
